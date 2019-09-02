417933d7ab0ba8bba0cbda5c7975ca2acad7b745
do <727809864@example.com>
2019-09-02 15:58:37 +0800

    update 'UiStack'

--- lib/bases/ui_stack.dart
+++ lib/bases/ui_stack.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 class UiStack extends NavigatorObserver {
   static final UiStack _instance = UiStack._internal();
-  static final List<Route> _routes = List();
   UiStack._internal();
   factory UiStack() => _instance;
 
+  static final List<Route> _routes = List();
   static List<Route> get routes => _routes;
+  static NavigatorState nav() => _instance.navigator;
 
   static void add(Widget widget) {
     _routes.add(MaterialPageRoute(builder: (context) => widget));
@@ -29,21 +30,25 @@ class UiStack extends NavigatorObserver {
 
     @override
     void didPop(Route route, Route previousRoute) {
+      print('route = $route, previousRoute = $previousRoute');
       _routes.remove(route);
     }
 
     @override
     void didPush(Route route, Route previousRoute) {
+      print('route = $route, previousRoute = $previousRoute');
       _routes.add(route);
     }
 
     @override
     void didRemove(Route route, Route previousRoute) {
+      print('route = $route, previousRoute = $previousRoute');
       _routes.remove(route);
     }
 
     @override
     void didReplace({Route newRoute, Route oldRoute}) {
+      print('newRoute = $newRoute, oldRoute = $oldRoute');
       final start = _routes.indexOf(oldRoute);
       if (start >= 0) {
         _routes.replaceRange(start, start + 1, [newRoute]);
