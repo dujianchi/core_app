cfb04f84dd0cf03061f12570458faae2a94ececb
do <727809864@example.com>
2019-09-05 16:30:17 +0800

    update 'Dimens'

--- lib/bases/dimens.dart
+++ lib/bases/dimens.dart
@@ -2,6 +2,7 @@ import 'package:flutter/widgets.dart';
 
 /// 等比缩放的屏幕适配方案
 class Dimens {
+  static const _default_screen_size = 360.0, _default_design_width = 720.0;
   static final Dimens _instance = Dimens._internal();
   Dimens._internal();
   factory Dimens() => _instance;
@@ -12,22 +13,37 @@ class Dimens {
       _instance._screenSize = MediaQuery.of(context).size.width;
 
   static set designWidth(double designWidth) =>
-      _instance._designWidth = designWidth;
+      _instance._designWidth = designWidth ?? _default_design_width;
+
+  static void init(BuildContext context, double designWidth) {
+    Dimens.screenSize = context;
+    Dimens.designWidth = designWidth;
+  }
 
   static double of(num px) => _instance._of(px);
 
   static int ofInt(num px) => _instance._ofInt(px);
 
-  final caches = <num, num>{};
-  double _screenSize, _designWidth;
+  double _screenSize = _default_screen_size,
+      _designWidth = _default_design_width;
+
+  // final Map<num, Map<num, num>> caches = {};
+  // double _of(num px) {
+  //   var forScreen = caches[_screenSize];
+  //   if (forScreen == null) {
+  //     forScreen = {};
+  //     caches[_screenSize] = forScreen;
+  //   }
+  //   double result = forScreen[px];
+  //   if (result == null) {
+  //     result = _screenSize * px / _designWidth;
+  //     forScreen[px] = result;
+  //   }
+  //   return result;
+  // }
 
   double _of(num px) {
-    double result = caches[px];
-    if (result == null) {
-      result = _designWidth == 0 ? px / 2 : _screenSize * px / _designWidth;
-      caches[px] = result;
-    }
-    return result;
+    return _screenSize * px / _designWidth;
   }
 
   int _ofInt(num px) => _of(px).round();
