958232b19f23dbdf77c05538340ef86ecb126eae
do <dujianchi512@gmail.com>
2019-11-10 21:20:27 +0800

    完善统一工具类

--- lib/bases/tools.dart
+++ lib/bases/tools.dart
@@ -1,4 +1,6 @@
-class StUtil {
+import 'package:intl/intl.dart';
+
+class Tools {
   /// string -> num
   static num toNum(String str, [num def]) {
     if (str?.isNotEmpty == true) {
@@ -16,5 +18,11 @@ class StUtil {
   /// string -> bool
   static bool toBool(String str) {
     return str == 'true' || str == '1' || str == '1.0';
+  }
+
+  /// 格式化日期，time为null则使用当前时间，format为null则使用yyyy-MM-dd HH:mm:ss
+  static String formatDate({String format, DateTime time}) {
+    return DateFormat(format ?? 'yyyy-MM-dd HH:mm:ss')
+        .format(time ?? DateTime.now());
   }
 }
