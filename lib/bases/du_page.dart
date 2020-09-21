4aab38d82fca8397fbfca3f00b8576fab73337ed
jianchi <727809864@qq.com>
2020-09-21 14:37:29 +0800

    test again +1

--- lib/bases/du_page.dart
+++ lib/bases/du_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:core_app/core.dart';
 import 'package:flutter/material.dart';
 
@@ -74,42 +76,35 @@ abstract class DuState<T extends DuStatefulWidget> extends State<T>
 
 ///作为默认主页样式
 abstract class DuPageBottomNav extends DuPage {
-  // tab bar
-  final Color bottomNavigationBarColor;
-  final int length;
-  final List<Widget> tabs;
-  final List<Widget> children;
-
-  DuPageBottomNav({
-    // tab bar
-    this.bottomNavigationBarColor,
-    this.length,
-    this.tabs,
-    this.children,
-  }) {
-    assert(length != null && tabs != null && children != null);
-    assert(
-        length > 0 && tabs.length == children.length && tabs.length == length);
-  }
+  DuPageBottomNav({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) => DefaultTabController(
-        length: tabChildren(context).length,
+        length:
+            min(tabChildren(context).length, tabViewChildren(context).length),
         child: super.build(context),
       );
 
   @override
   Widget bottomNavigationBar(context) => Material(
-        color: bottomNavigationBarColor ?? Theme.of(context).primaryColor,
+        color:
+            bottomNavigationBarColor(context) ?? Theme.of(context).primaryColor,
         child: TabBar(
-          tabs: tabs,
+          tabs: tabChildren(context),
         ),
       );
 
   @override
   Widget buildChild(BuildContext context) => TabBarView(
-        children: tabChildren(context),
+        children: tabViewChildren(context),
       );
 
+  //tab按钮颜色(位于底部)
+  Color bottomNavigationBarColor(BuildContext context) => Colors.white70;
+
+  /// tab内容布局(位于上部)
+  List<Widget> tabViewChildren(BuildContext context);
+
+  /// tab按钮布局(位于底部)
   List<Widget> tabChildren(BuildContext context);
 }
